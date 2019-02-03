e0cd3b414f50d1ecf86af37be05a446cc2b8ac2a
Kaephas <kaephask@yahoo.com>
2019-02-03 13:58:15 -0800

    first try

---
+++ testPHP.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>$Title$</title>
+</head>
+<body>
+$END$
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaeph
+ * Date: 2/3/2019
+ * Time: 13:57
+ */
